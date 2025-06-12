537bc5f7adaf8eea6ed16b9bbaaa8e1444012604
Afifa Fatima <afifarizvi7@gmail.com>
2025-06-12 23:31:54 +0530

    Added seperation of concerns in model and created owner model

---
+++ models/ownermodel.js
@@ -0,0 +1,21 @@
+const mongoose=require('mongoose');
+
+
+
+const ownerSchema=mongoose.mongoose.Schema({
+    fullname:String,
+    email:String,
+    password:String,
+    
+    
+    products:{
+        type: Array,
+        default:[],
+    },
+   
+    picture:String,
+    gstin:String,
+
+});
+
+module.exports=mongoose.model("owner",ownerSchema);
